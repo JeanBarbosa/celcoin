d1af1071797ef11f167b139b5b42d8ce466e735f
Jean Barbosa <programmer.jean@gmail.com>
2020-07-21 13:19:26 -0300

    tests(fix): fix

--- tests/integration/celcoin.spec.ts
+++ tests/integration/celcoin.spec.ts
@@ -1,35 +1,32 @@
+import { IProvider } from '../../lib/topups';
 import { Celcoin } from '../../index';
 
 describe('Class Celcoin', () => {
-
   const celcoin = new Celcoin('teste', 'teste');
 
   it('Get all Providers', async () => {
+    const { providers } = await celcoin.topup.getProviders({
+      stateCode: 61,
+    });
 
-    const { providers } = await celcoin.topup.getProviders(61);
-
-    const provider = {
-        category: 1,
-        name: "Claro",
-        providerId: 2087,
-        RegionaisnameProvider: [],
-        TipoRecarganameProvider: 2,
-        maxValue: 0.0,
-        minValue: 0.0
+    const provider: IProvider = {
+      category: 1,
+      name: 'Claro',
+      providerId: 2087,
+      RegionaisnameProvider: [],
+      TipoRecarganameProvider: 2,
+      maxValue: 0.0,
+      minValue: 0.0,
     };
 
     expect(providers).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining(provider)
-      ])
-    )
-
-  })
+      expect.arrayContaining([expect.objectContaining(provider)]),
+    );
+  });
 
   it('Retrieve your balance info', async () => {
     const response = await celcoin.merchant.balance();
 
-    expect(response).toMatchObject({ balance: expect.any(Number)});
-
-  })
+    expect(response).toMatchObject({ balance: expect.any(Number) });
+  });
 });
--- tests/unit/topups.spec.ts
+++ tests/unit/topups.spec.ts
@@ -1,47 +1,41 @@
 import Topups from '../../lib/topups';
 
 describe('Class Topups', () => {
-
   const topup = new Topups('teste', 'teste');
 
   it('Get a list of providers', async () => {
+    const { providers } = await topup.getProviders({ stateCode:  61});
 
-    const { providers } = await topup.getProviders(61);
-    
     const provider = {
       category: 1,
-      name: "Claro",
+      name: 'Claro',
       providerId: 2087,
       RegionaisnameProvider: [],
       TipoRecarganameProvider: 2,
       maxValue: 0.0,
-      minValue: 0.0
+      minValue: 0.0,
     };
 
     expect(providers).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining(provider)
-      ])
-    )
-
-  })
+      expect.arrayContaining([expect.objectContaining(provider)]),
+    );
+  });
 
   it('Find Provider', async () => {
-    const response = await topup.findProvider(61, '999999999');
+    const response = await topup.findProvider({stateCode: 61, phoneNumber: '999999999'});
 
     const expected = {
-      nameProvider: "Vivo",
+      nameProvider: 'Vivo',
       providerId: 2088,
-      errorCode: "000",
-      message: "SUCESSO",
-      status: 0
-    }
+      errorCode: '000',
+      message: 'SUCESSO',
+      status: 0,
+    };
 
     expect(response).toEqual(expected);
-  })
+  });
 
   it('Retrieve a list of Providers and its values', async () => {
-
     const response = await topup.getProviderValues(61, 2087);
 
     const { value } = response;
@@ -50,52 +44,47 @@ describe('Class Topups', () => {
       properties: null,
       code: 0,
       cost: 0.0,
-      detail: "",
-      productName: "R$ 13,00",
+      detail: '',
+      productName: 'R$ 13,00',
       checkSum: -2147483640,
       dueProduct: 30,
       valueBonus: 0.0,
       maxValue: 13.0,
-      minValue: 13.0
+      minValue: 13.0,
     };
 
     expect(value).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining(
-          expected
-        )
-      ])
-    )
+      expect.arrayContaining([expect.objectContaining(expected)]),
+    );
   });
 
+  /*
   it('Topups', async () => {
-    
     const body = {
-      "externalTerminal": "1234567",
-      "topupData": {
-         "paymentMethod": "DINHEIRO",
-        "pontos": 0,
-        "value": 20.0,
-        "originalValue": 0,
-        "valueWithDiscount": 0,
-        "valueWithAdditional": 0
+      externalTerminal: '1234567',
+      topupData: {
+        paymentMethod: 'DINHEIRO',
+        pontos: 0,
+        value: 20.0,
+        originalValue: 0,
+        valueWithDiscount: 0,
+        valueWithAdditional: 0,
       },
-      "cpfCnpj": "11036382702",
-     "signerCode":  "11415044805",
-     "providerId": 2087,
-     "phone": {
-       "stateCode": 61,
-       "countryCode": 55,
-       "number": "993576555"
-     }
+      cpfCnpj: '11036382702',
+      signerCode: '11415044805',
+      providerId: 2087,
+      phone: {
+        stateCode: 61,
+        countryCode: 55,
+        number: '993576555',
+      },
     };
 
     const response = await topup.new(body);
 
-    expect(response).toMatchObject({ transactionId: expect.any(Number)});
-
-  })
-
+    expect(response).toMatchObject({ transactionId: expect.any(Number) });
+  });
+ */
   /*
   it('Capture a posted topup transaction', async () => {
     const response = await topup.capture(3830743);
@@ -108,5 +97,4 @@ describe('Class Topups', () => {
     expect(response).toEqual(expected);
 
   }) */
-
 });
